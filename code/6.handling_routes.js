0d30c5f2ae28e2bb1d0442df7bbd9ae5fb7b04a4
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2023-09-27 20:44:56 +0530

    feat: handing routes in express

---
+++ code/6.handling_routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>hello from Express!</h1>");
+});
+app.listen(3000);
